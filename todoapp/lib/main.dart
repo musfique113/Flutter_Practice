7326be4671dffaac67a169257cceebf3ac5df2dd
musfique113 <musfique113@gmail.com>
2023-03-27 00:01:16 +0600

    initialCommit

---
+++ todoapp/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+// import 'package:todoapp/screens/home.dart';
+
+void main() {
+  runApp(Center(child: const Text(
+    'Hel dsfvsdfssfddsf  dhakd SDASDF',
+    textDirection: TextDirection.ltr,
+  ),));
+}
