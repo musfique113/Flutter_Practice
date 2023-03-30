ba91347a2e7e7a8718f83db6ac30bea456746a99
musfique113 <musfique113@gmail.com>
2023-03-30 16:21:16 +0600

    test

--- todoapp/lib/main.dart
+++ todoapp/lib/main.dart
@@ -1,9 +1,46 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-// import 'package:todoapp/screens/home.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(Center(child: const Text(
-    'Hel dsfvsdfssfddsf  dhakd SDASDF',
-    textDirection: TextDirection.ltr,
-  ),));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('A random idea:'),
+          Text(appState.current.asLowerCase),
+        ],
+      ),
+    );
+  }
 }
