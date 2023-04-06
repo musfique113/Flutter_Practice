512152f73909b65bec28917ef16a543590deb2fb
musfiqe113 <musfique113@gmail.com>
2023-04-07 01:34:44 +0600

    ecommers

---
+++ ecommerceyt/lib/screens/loginScreen/login_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  Widget buildTopPart() {
+    return Column(
+      children: [Image.asset("images/Logo.png")],
+    );
+  }
+// this is for logo 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: ListView(
+          physics: BouncingScrollPhysics(),
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [buildTopPart()],
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
