2f376d2f58717a0b26be6f93a3e03634bdeafdb9
Mahfujur Rahman Musfique <musfique113@gmail.com>
2023-06-04 18:59:57 +0600

    added the assignment for module 5

---
+++ OstadFlutterProject-Assignments/ModuleFive/android/app/src/main/java/com/ostad/flutterbatchthree/module_five_assignment/MainActivity.java
@@ -0,0 +1,6 @@
+package com.ostad.flutterbatchthree.module_five_assignment;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- xyx/android/app/src/main/kotlin/com/example/xyx/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.xyx
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
