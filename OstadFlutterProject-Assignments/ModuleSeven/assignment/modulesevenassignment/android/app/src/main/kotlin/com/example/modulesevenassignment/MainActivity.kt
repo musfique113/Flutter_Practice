45cca58bf2db4cf6bc232f638c85fe6f52decafa
Mahfujur Rahman Musfique <musfique113@gmail.com>
2023-06-18 18:24:04 +0600

    added module7 assignment

---
+++ OstadFlutterProject-Assignments/ModuleSeven/assignment/modulesevenassignment/android/app/src/main/kotlin/com/example/modulesevenassignment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.modulesevenassignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ OstadFlutterProject-Assignments/ModuleSeven/assignment/modulesevenassignment/lib/home_screen.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+
+class Product {
+  String name;
+  double price;
+  int counter;
+
+  Product({required this.name, required this.price, this.counter = 0});
+}
+
+class ProductList extends StatefulWidget {
+  @override
+  _ProductListState createState() => _ProductListState();
+}
+
+class _ProductListState extends State<ProductList> {
+  List<Product> products = [
+    Product(name: 'Apple', price: 10.0),
+    Product(name: 'Lime', price: 20.0),
+    Product(name: 'Strawberry', price: 30.0),
+  ];
+
+  int totalProducts = 0;
+
+  void incrementCounter(int index) {
+    setState(() {
+      products[index].counter++;
+      totalProducts++;
+      if (products[index].counter == 5) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Congratulations!'),
+              content: Text(
+                  'You\'ve bought 5 ${products[index].name}!'),
+              actions: <Widget>[
+                ElevatedButton(
+                  child: Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product List'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CartPage(totalProducts: totalProducts),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(products[index].name),
+            subtitle: Text('\$${products[index].price.toStringAsFixed(2)}'),
+            trailing: ElevatedButton(
+              onPressed: () => incrementCounter(index),
+              child: Text('Buy Now'),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class CartPage extends StatelessWidget {
+  final int totalProducts;
+
+  CartPage({required this.totalProducts});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cart'),
+      ),
+      body: Center(
+        child: Text('Total products bought: $totalProducts'),
+      ),
+    );
+  }
+}
