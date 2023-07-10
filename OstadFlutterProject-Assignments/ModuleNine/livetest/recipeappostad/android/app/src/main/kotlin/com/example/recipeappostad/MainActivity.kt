4171a306af3bb0dbeffeb1d1d6820e1187dbcdb9
Mahfujur Rahman Musfique <musfique113@gmail.com>
2023-07-10 23:21:13 +0600

    added the module 9 live test

---
+++ OstadFlutterProject-Assignments/ModuleNine/livetest/recipeappostad/android/app/src/main/kotlin/com/example/recipeappostad/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.recipeappostad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ OstadFlutterProject-Assignments/ModuleNine/livetest/recipeappostad/lib/home_screen.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'model.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late List<Recipe> recipes;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    final jsonString =
+        await rootBundle.loadString('assets/api/recipi-ostasd-modulenine.json');
+    final jsonData = json.decode(jsonString);
+    final recipeList = jsonData['recipes'] as List<dynamic>;
+    recipes = recipeList.map((json) => Recipe.fromJson(json)).toList();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Recipes'),
+      ),
+      body: recipes != null
+          ? ListView.builder(
+              itemCount: recipes.length,
+              itemBuilder: (context, index) {
+                final recipe = recipes[index];
+                return ListTile(
+                  title: Text(recipe.title),
+                  subtitle: Text(recipe.description),
+                  onTap: () {
+                  },
+                );
+              },
+            )
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
+    );
+  }
+}
