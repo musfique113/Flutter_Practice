4fa4648e319024a5126ef218d5046b946ee2b832
Mahfujur Rahman Musfique <musfique113@gmail.com>
2023-07-16 19:05:56 +0600

    completed the basic tasks, will work later

---
+++ OstadFlutterProject-Assignments/ModuleTen/assignment/orientationapp/README.md
@@ -0,0 +1,8 @@
+# orientationapp
+
+
+
+## Screenshots
+
+![landscape](https://github.com/musfique113/Flutter_Practice/assets/53111065/57829ccd-f065-4fa5-8b6a-d456bac64165)
+![proitate](https://github.com/musfique113/Flutter_Practice/assets/53111065/7b57949d-b6a4-49a2-9f8a-276031d88f5d)
---
+++ OstadFlutterProject-Assignments/ModuleTen/assignment/orientationapp/lib/home_sceen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Orientation'),
+      ),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return orientation == Orientation.portrait
+              ? buildPortraitLayout()
+              : buildLandscapeLayout();
+        },
+      ),
+    );
+  }
+
+  Widget buildPortraitLayout() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        CircleAvatar(
+          radius: 60,
+          backgroundImage: NetworkImage(
+              'https://avatars.githubusercontent.com/u/53111065?v=4'),
+        ),
+        SizedBox(height: 10),
+        Text(
+          'Mahfujur R. Musfique',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 10),
+        Text(
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(height: 20),
+        Expanded(
+          child: buildImageGridView(),
+        ),
+      ],
+    );
+  }
+
+  Widget buildLandscapeLayout() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 60,
+                backgroundImage: NetworkImage(
+                    'https://avatars.githubusercontent.com/u/53111065?v=4'),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Mahfujur R. Musfique',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Dummy discription .',
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: buildImageGridView(),
+        ),
+      ],
+    );
+  }
+
+  Widget buildImageGridView() {
+    return GridView.count(
+      crossAxisCount: 3,
+      children: List.generate(9, (index) {
+        return Image.network(
+          'https://avatars.githubusercontent.com/u/53111065?v=4',
+          fit: BoxFit.cover,
+        );
+      }),
+    );
+  }
+}
