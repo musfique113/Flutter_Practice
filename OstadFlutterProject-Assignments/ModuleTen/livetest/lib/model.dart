f92b7a945af2d08190be12256b745ff62d445ad4
Mahfujur Rahman Musfique <musfique113@gmail.com>
2023-07-17 23:31:30 +0600

    updated the read me

--- OstadFlutterProject-Assignments/ModuleTen/livetest/lib/model.dart
+++ OstadFlutterProject-Assignments/ModuleTen/livetest/lib/model.dart
@@ -1,7 +1,8 @@
 final List<String> newsArticles = [
   'https://images.pexels.com/photos/935979/pexels-photo-935979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/373076/pexels-photo-373076.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/2929993/pexels-photo-2929993.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/3756760/pexels-photo-3756760.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/1051745/pexels-photo-1051745.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  'https://images.pexels.com/photos/935979/pexels-photo-935979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/935979/pexels-photo-935979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/935979/pexels-photo-935979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/935979/pexels-photo-935979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/935979/pexels-photo-935979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 ];
